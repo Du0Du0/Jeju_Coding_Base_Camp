f238b558c3c80afcdca906ba9481f956d408360b
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 <dohyun911@hanmail.net>
2023-04-21 02:03:02 +0900

    JS 100제 풀이

---
+++ 03.변수의 타입.js.js
@@ -0,0 +1,7 @@
+// 문제3 : 변수의 타입
+// 다음 출력 값으로 올바른 것은?
+
+var arr = [100, 200, 300];
+console.log(typeof(arr));
+
+답 : object
--- 04.변수의 타입.js.js
+++
@@ -1,3 +0,0 @@
-var arr = [100, 200, 300];
-console.log(typeof(arr));
-object
