08c88a952d212451db5aaaaec9743baa27275540
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 <dohyun911@hanmail.net>
2023-04-21 11:25:05 +0900

    JS 100제 풀이

---
+++ 30.문자열 속 문자 찾기.js
@@ -0,0 +1,20 @@
+// 문제 30 문자열 속 문자 찾기
+// 문자 pineapple에는 apple이라는 문자가 숨어 있습니다. 원범이는 이렇듯 문자열 속에 숨어있는 문자를 찾아보려고 합니다.
+
+// 첫번째 입력에서는 문자열이 입력되고, 두번째에는 찾을 문자가 입력되어야 합니다.
+// 그 문자가 시작하는 index를 반환하는 프로그램을 만들어 주세요
+
+// 입력
+// pineapple is yummy
+// apple
+
+// 출력
+// 4
+
+const n = prompt('영어문장을 입력해주세요').split('');
+const findWord = prompt('찾을 영어단어를 입력해주세요');
+const result = n.findIndex((element) => element === findWord[0]);
+console.log(result);
+
+
+
