1c53d15508e863185e43821c51373d38f5aa40d1
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 <dohyun911@hanmail.net>
2023-04-21 11:35:36 +0900

    JS 100제 풀이

--- 29 .대문자만 지나가세요.js
+++ 29 .대문자만 지나가세요.js
@@ -2,7 +2,7 @@
 // 진구는 영어 학원 아르바이트를 하고 있습니다. 반 아이들은 알파벳을 공부하는 학생들인데 오늘은 대문자 쓰기 시험을 봤습니다.
 // 알파벳 하나만을 입력하고 그 알파벳이 대문자이면 YES를 아니면 NO를 출력하는 프로그램을 만들어 주세요.
 
-
+//방법 1
 function alpabet(n){
     if(n === n.toUpperCase() ){
         return 'YES';
@@ -13,3 +13,12 @@ function alpabet(n){
 
 const n = prompt('알파벳 하나를 쓰시오.');
 console.log(alpabet(n));
+
+// 방법 2
+const data = prompt('알파벳을 입력하세요.');
+
+if (data === data.toUpperCase()){
+  console.log('YES');
+} else {
+  console.log('NO');
+}
